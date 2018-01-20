9fd3883e196e36665e54ef78bd0fd6e43f79694d
ahmed fadhil <fadhil@fbk.eu>
2018-01-20 14:09:05 +0100

    added new activites and updated Maven

--- ParseStarterProject/src/main/java/com/parse/starter/UserList.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserList.java
@@ -1,19 +1,34 @@
 package com.parse.starter;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ParseException;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class UserList extends AppCompatActivity {
 
@@ -31,6 +46,14 @@ public class UserList extends AppCompatActivity {
 
         final ListView userList = (ListView) findViewById(R.id.userList);
 
+
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
         ParseQuery<ParseUser> query = ParseUser.getQuery();
 
         query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
@@ -45,10 +68,9 @@ public class UserList extends AppCompatActivity {
                     if (objects.size() > 0) {
 
 
-
                         for (ParseUser user : objects) {
 
-                                usernames.add(user.getUsername());
+                            usernames.add(user.getUsername());
 
                         }
 
@@ -58,13 +80,10 @@ public class UserList extends AppCompatActivity {
                     }
 
 
-
                 }
 
             }
         });
-
-
 
 
     }
@@ -84,10 +103,60 @@ public class UserList extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.share) {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 1);
+
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+
+            try {
+
+                Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+//                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//                imageView.setImageBitmap(bitmapImage);
+
+                Log.i("AppInfo", "Image received");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                ParseFile file = new ParseFile("image.png", byteArray);
+                ParseObject object = new ParseObject("images");
+                object.put("username", ParseUser.getCurrentUser().getUsername());
+                object.put("images", file);
+                object.saveInBackground(new SaveCallback()) {
+                    @Override
+                    public void done (ParseException e){
+                        if (e == null) {
+                            Toast.makeText(getApplication().getBaseContext(), "Your image has beed posted!", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(getApplication().getBaseContext(), "There was an error - Please try again later.", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+
+                });
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplication().getBaseContext(), "There was an error - Please try again later.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
